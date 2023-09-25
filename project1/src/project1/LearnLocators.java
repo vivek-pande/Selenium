21396a67273b5e9c3f1c20ee8b08af48b6b79798
vivek-pande <pandeyvivek033@gmail.com>
2023-09-25 20:05:14 +0530

    more locators added

--- project1/src/project1/LearnLocators.java
+++ project1/src/project1/LearnLocators.java
@@ -11,7 +11,7 @@ public class LearnLocators {
 	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		/*
 		 * The Implicit Wait in Selenium is used to tell the web driver to wait for a
 		 * certain amount of time before it throws a “No Such Element Exception”. The
@@ -23,15 +23,32 @@ public class LearnLocators {
 		driver.findElement(By.id("inputUsername")).sendKeys("vivek");
 		driver.findElement(By.name("inputPassword")).sendKeys("abcd");
 		driver.findElement(By.className("signInBtn")).click();
-//		String errorMessage =  driver.findElement(By.cssSelector("p.error")).getText();
-//		String errorMessage = driver.findElement(By.className("error")).getText();
+		// String errorMessage =
+		// driver.findElement(By.cssSelector("p.error")).getText();
+		// String errorMessage = driver.findElement(By.className("error")).getText();
 		String errorMessage = driver.findElement(By.cssSelector("p[class=\"error\"]")).getText();
 		System.out.println(errorMessage);
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		driver.findElement(By.cssSelector("input[placeholder=\"Name\"]")).sendKeys("vivek pandey");
 		driver.findElement(By.cssSelector("input[placeholder=\"Email\"]")).sendKeys("vffivek@gmail.com");
-		driver.findElement(By.cssSelector("input[placeholder=\"Phone Number\"]")).sendKeys("701122189");
-		
+		// driver.findElement(By.cssSelector("input[placeholder=\"Phone
+		// Number\"]")).sendKeys("701122189");
+		// driver.findElement(By.cssSelector("input[type='text']:nth-child(4)")).sendKeys("4984794");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("4984794453");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.findElement(By.cssSelector(".infoMsg")).getText());
+		driver.findElement(By.className("go-to-login-btn")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("input[placeholder*=User]")).sendKeys("vivek");
+		driver.findElement(By.cssSelector("input[placeholder*=Pass]")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.id("chkboxTwo")).click();
+		driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.findElement(By.cssSelector("h2")).getText()
+				+" "+ driver.findElement(By.cssSelector("p")).getText());
 
 	}
 
