2d8284e1a37c5ea907d76cc56287302c7c5fd682
vivek-pande <pandeyvivek033@gmail.com>
2023-09-29 18:05:03 +0530

    dyanamic dropdown added

---
+++ project1/src/webElementsPractise/AutoSuggestiveDropdown.java
@@ -0,0 +1,10 @@
+package webElementsPractise;
+
+public class AutoSuggestiveDropdown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project1/src/webElementsPractise/DynamicDropdown.java
@@ -0,0 +1,28 @@
+package webElementsPractise;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+//		driver.findElement(By.xpath("//a[@text=\"Bhopal (BHO)\"]")).click();
+		driver.findElement(
+				By.xpath("//div[@id=\"ctl00_mainContent_ddl_originStation1_CTNR\"] //a[@text=\"Bengaluru (BLR)\"]"))
+				.click();
+		Thread.sleep(2000);
+//		driver.findElement(By.xpath("(//a[@text=\"Amritsar (ATQ)\"])[2]")).click();
+		driver.findElement(By.xpath("//div[@id=\"ctl00_mainContent_ddl_destinationStation1_CTNR\"] //a[@text=\"Bhopal (BHO)\"]")).click();
+
+	}
+
+}
